cd1104dc7850e15bd5845942380e87607924b36e
nanni00 <giovanni.malaguti00@gmail.com>
2024-03-28 12:58:18 +0100

    Modified tokenization and embeddings functions; basic time testing

--- src/code/fasttext/embedding_utils.py
+++ src/code/fasttext/embedding_utils.py
@@ -1,4 +1,5 @@
 import re
+import string
 import numpy as np
 import pandas as pd
 
@@ -10,19 +11,24 @@ import fasttext
 
 from nltk.corpus import stopwords
 
-
+# tr = str.maketrans('', '', string.punctuation.replace('-', '')) # to keep minus sign
 stopwords_set = set(stopwords.words('english'))
 
+
 def my_tokenizer(s: str, keepnumbers=True):
-    if type(s) is not str:
-        return str(s)
-    # ISSUE: this return any substring with 1 to 4 digits, for example
-    # 369155,29421 --> ['3691', '55', '2942', '1']
-    # maybe it's better to return a number only if it's originally between 0 and 9999...
+    #if type(s) is not str:
+    #    return str(s)
+    s = str(s)
     if keepnumbers:
-        return [x for x in re.findall(r'[a-z]+|[0-9]{1,4}', s.lower()) if x not in stopwords_set]
+        return [            
+            x for x in re.findall(r'\b([a-z]+|\d{1}|\d{2}|\d{3}|\d{4})\b', s.lower()) 
+            if x not in stopwords_set
+        ]
     else:
-        return [x for x in re.findall(r'[a-z]+', s.lower()) if x not in stopwords_set]
+        return [
+            x for x in re.findall(r'[a-z]+', s.lower())
+            if x not in stopwords_set
+        ]
 
 
 def np_cosine_similarity(a1, a2):
@@ -78,39 +84,57 @@ class TableEncoder:
     def embedding_row(self, row: list):
         return \
             np.mean(
-                [self.embedding_cell(cell) for cell in row],
+                np.array(
+                    [self.embedding_cell(cell) for cell in row if type(cell) in [str, list] and len(cell) > 0],
+                ),
                 axis=0
             )
 
     def embedding_column(self, column: list):
         return \
             np.mean(                
-                np.array([
-                    self.embedding_cell(cell) 
-                    for cell in column
-                ]),
+                np.array(
+                    [self.embedding_cell(cell) for cell in column if type(cell) in [str, list] and len(cell) > 0]
+                ),
                 axis=0
             )
 
     def create_row_embeddings(self, df: pd.DataFrame, add_label=False, keepnumbers=True):
         labels_embedding = list(map(my_tokenizer, df.columns)) if add_label else None
-        return \
-            (     
-                self.embedding_row(row.apply(my_tokenizer, args=(keepnumbers, )).to_list()) if not add_label \
-                    else self.embedding_row(row.apply(my_tokenizer, args=(keepnumbers, )).to_list() + labels_embedding) 
-                
-                 for _, row in df.iterrows()
-            )
+        embeddings = []
+        for _, row in df.iterrows():
+            if add_label:
+                e = self.embedding_row(row.apply(my_tokenizer, args=(keepnumbers, )).to_list() + labels_embedding)
+            else:
+                e = self.embedding_row(row.apply(my_tokenizer, args=(keepnumbers, )).to_list())
+
+            if type(e) is np.float64: # case the embedding is nan
+                embeddings.append(np.zeros(self.model_size))
+            else:
+                embeddings.append(e)
+        return embeddings 
+        #return \
+        #    (     
+        #        self.embedding_row(row.apply(my_tokenizer, args=(keepnumbers, )).to_list()) if not add_label \
+        #            else self.embedding_row(row.apply(my_tokenizer, args=(keepnumbers, )).to_list() + labels_embedding) 
+        #        
+        #         for _, row in df.iterrows()
+        #    )
     
     def create_column_embeddings(self, df: pd.DataFrame, add_label=False, keepnumbers=True):
-        return \
-            (
-                self.embedding_column(df[column].apply(my_tokenizer, args=(keepnumbers, )).to_list()) \
-                    if not add_label \
-                    else self.embedding_column(df[column].apply(my_tokenizer, args=(keepnumbers, )).to_list() + [my_tokenizer(df[column].name, keepnumbers)])
-                for column in df.columns
-            )
+        embeddings = []
+        for column in df.columns:
+            if add_label:
+                e = self.embedding_column(df[column].apply(my_tokenizer, args=(keepnumbers, )).to_list() + [my_tokenizer(df[column].name, keepnumbers)])
+            else:
+                e = self.embedding_column(df[column].apply(my_tokenizer, args=(keepnumbers, )).to_list())
 
+            if type(e) is np.float64:
+                embeddings.append(np.zeros(self.model_size))
+            else:
+                embeddings.append(e)
+        
+        return embeddings
 
 
 def compare_embeddings_of(df1: pd.DataFrame, df2: pd.DataFrame,
@@ -130,8 +154,8 @@ def compare_embeddings_of(df1: pd.DataFrame, df2: pd.DataFrame,
     
     if on == 'columns':
         comparisons = pd.DataFrame(columns=['C1', 'C2', 'cosine similarity'])
-        emb1 = list(tabenc.create_column_embeddings(df1, add_label, keepnumbers))
-        emb2 = list(tabenc.create_column_embeddings(df2, add_label, keepnumbers))
+        emb1 = tabenc.create_column_embeddings(df1, add_label, keepnumbers)
+        emb2 = tabenc.create_column_embeddings(df2, add_label, keepnumbers)
         for i, column1 in enumerate(df1.columns):
             for j, column2 in enumerate(df2.columns):
 
@@ -140,8 +164,8 @@ def compare_embeddings_of(df1: pd.DataFrame, df2: pd.DataFrame,
                 comparisons.loc[len(comparisons)] = [column1, column2, cosim]
     else:
         comparisons = pd.DataFrame(columns=['R1', 'R2', 'cosine similarity'])
-        emb1 = list(tabenc.create_row_embeddings(df1, add_label, keepnumbers))
-        emb2 = list(tabenc.create_row_embeddings(df2, add_label, keepnumbers))
+        emb1 = tabenc.create_row_embeddings(df1, add_label, keepnumbers)
+        emb2 = tabenc.create_row_embeddings(df2, add_label, keepnumbers)
         for i, emb_s in enumerate(emb1):
             for j, emb_p in enumerate(emb2):
                 cosim = np_cosine_similarity(emb_s, emb_p)
@@ -156,7 +180,7 @@ def show_most_similar_rows(compared_rows: pd.DataFrame,
                            df1: pd.DataFrame, df2: pd.DataFrame, 
                            n=5, sorted=True):
     if not sorted:
-        compared_rows = compared_rows.sort_values(by='cosine similarity', ascending=False, ignore_index=True)
+        compared_rows = compared_rows.sort_values(by=compared_rows.columns[2], ascending=False, ignore_index=True)
 
     for i in range(n):
         r1, r2, cosim = compared_rows.loc[i]
